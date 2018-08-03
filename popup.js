c3bfd05c4097b626cd663322355e53ff8c72571f
Ethan Fung <ethanefung@gmail.com>
2018-08-02 20:51:30 -0700

    fix input implicit submit type bug

--- popup.js
+++ popup.js
@@ -1,13 +1,17 @@
 const timerSelectors = ['browserTimer', 'fbTimer'];
 
-document.addEventListener('DOMContentLoaded', renderPopup);
 chrome.storage.onChanged.addListener(renderPopup);
 chrome.browserAction.onClicked.addListener(renderPopup);
 
+document.body.addEventListener('DOMContentLoaded', e => e.stopPropagation() && renderPopup(), false);
+document.body.querySelector('#initTimer').addEventListener('click', revealAddTimerForm, false);
+document.body.querySelector('#addDomainInput').addEventListener('click', addDomain, false);
+document.body.querySelector('#submitTimer').addEventListener('click', addTimer, false);
+
 function renderPopup() {
-  chrome.storage.local.get(null, function(result) {
-    timerSelectors.forEach($timer => renderTimer($timer, result));
-  });
+  chrome.storage.local.get(null, 
+    result => timerSelectors.forEach($timer => renderTimer($timer, result))
+  );
 }
 
 function renderTimer(selector, result) {
@@ -25,3 +29,47 @@ function renderTimer(selector, result) {
   document.getElementById(selector).textContent = 
     `${time.hours}:${time.minutes}:${time.seconds}`;
 }
+
+function revealAddTimerForm(e) {
+  e.stopPropagation();
+  e.preventDefault();
+  
+  const $form = document.querySelector('#addTimerForm');
+  const $initBtn = document.querySelector('#initTimer');
+  if ($form.hasAttribute('hidden')) {
+    $form.removeAttribute('hidden');
+    $initBtn.setAttribute('hidden', true);
+  } else {
+    $form.setAttribute('hidden', true);
+    $initBtn.removeAttribute('hidden');
+  }
+}
+
+function addDomain(e) {
+  e.stopPropagation();
+  e.preventDefault();
+  const $domain = document.createElement('li');
+  const $input = document.createElement('input');
+  
+  
+  $domain.append($input, $cancelFactory());
+  $domain.setAttribute('class', 'timerDomain');
+  document.querySelector('#timerDomains').appendChild($domain);
+
+  function $cancelFactory() {
+    const $cancel = document.createElement('button');
+    $cancel.textContent = 'x';
+    $cancel.setAttribute('type', 'button');
+    $cancel.addEventListener('click', function(e) {
+      e.preventDefault();
+      document.body.querySelector('#timerDomains').removeChild($cancel.parentNode);
+    });
+    return $cancel;
+  }
+}
+
+function addTimer(e) {
+  e.stopPropagation();
+  e.preventDefault();
+}
+
